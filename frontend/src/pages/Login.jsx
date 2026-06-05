09420f906069c31564e82c6f73dc4bb726210962
Urbain Yaovi DOHOUNGBO <urbanodohoungbo@gmail.com>
2026-06-05 20:52:28 +0000

    corrections Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,99 +1,70 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
-
 import { AuthContext } from "../context/AuthContext";
-
 import API from "../services/api";
+import "../pages/Login.css";
 
 function Login() {
-
   const navigate = useNavigate();
-
   const { login } = useContext(AuthContext);
 
   const [formData, setFormData] = useState({
     email: "",
-    password: ""
+    password: "",
   });
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   const handleSubmit = async (e) => {
-
     e.preventDefault();
-
     try {
-
-      const response = await API.post(
-        "/users/login",
-        formData
-      );
-
+      const response = await API.post("/users/login", formData);
       login(response.data);
-
       navigate("/dashboard");
-
     } catch (error) {
-
       alert(
-        error.response?.data?.message ||
-        "Erreur de connexion"
+        error.response?.data?.message || "Erreur de connexion"
       );
-
     }
   };
 
   return (
-  <div>
-    <h1>Connexion</h1>
+    <div className="login-page">
+      <div className="login-container">
+        <h1>Connexion</h1>
 
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        onChange={handleChange}
-        required
-      />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={handleChange}
+          />
 
-      <br />
-      <br />
+          <input
+            type="password"
+            name="password"
+            placeholder="Mot de passe"
+            onChange={handleChange}
+          />
 
-      <input
-        type="password"
-        name="password"
-        placeholder="Mot de passe"
-        onChange={handleChange}
-        required
-      />
+          <button type="submit">Se connecter</button>
+        </form>
 
-      <br />
-      <br />
-
-      <button type="submit">
-        Se connecter
-      </button>
-    </form>
-
-    <br />
-
-    <p>
-      Vous n'avez pas de compte ?
-    </p>
-
-    <button
-      onClick={() => navigate("/register")}
-    >
-      Créer un compte
-    </button>
-  </div>
-);
+        <div className="register-link">
+          <p>Vous n'avez pas de compte ?</p>
+          <button onClick={() => navigate("/register")}>
+            Créer un compte
+          </button>
+        </div>
+      </div>
+    </div> 
+  );
 }
 
-export default Login;
+export default Login; 

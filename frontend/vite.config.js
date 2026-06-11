5d2823158f2ed17130d7cc5c0656922eb671cee2
Urbain Yaovi DOHOUNGBO <urbanodohoungbo@gmail.com>
2026-06-11 22:30:16 +0000

    fix vite build target

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    // ✅ ignore les erreurs ESLint pendant le build
-    rollupOptions: {
-      onwarn(warning, warn) {
-        if (warning.code === 'UNUSED_EXTERNAL_IMPORT') return;
-        warn(warning);
-      }
-    }
+    target: 'es2015'
   }
 })

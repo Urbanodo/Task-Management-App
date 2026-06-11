12ba07ab5978963f500aa454155dceb5ced1eb00
Urbain Yaovi DOHOUNGBO <urbanodohoungbo@gmail.com>
2026-06-11 22:04:29 +0000

    fix vite config pour Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // ✅ ignore les erreurs ESLint pendant le build
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'UNUSED_EXTERNAL_IMPORT') return;
+        warn(warning);
+      }
+    }
+  }
 })

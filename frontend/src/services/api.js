9cbba34500dc992e395fea2303b3e0643d62eaa6
Urbain Yaovi DOHOUNGBO <urbanodohoungbo@gmail.com>
2026-06-12 05:38:58 +0000

    fix baseURL api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://gestion-des-taches-backend.onrender.com",
+  baseURL: "https://gestion-des-taches-backend.onrender.com/api",
 });
 
 export default API;
